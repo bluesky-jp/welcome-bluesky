f979ea241084a54881865cf97f979182d4e25fdc
Akihiro Urushiara <a.urusihara@gmail.com>
2023-08-20 12:56:31 +0900

    歩き方についてのドキュメントを記載中

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
